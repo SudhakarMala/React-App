f4e6c9a7302b4d314099df99505d5461499b873e
lucky <lucky@DESKTOP-1057CDB>
2024-10-05 15:54:35 +0530

    Practiced

--- src/Practice/DataBinding.jsx
+++ src/Practice/DataBinding.jsx
@@ -1,12 +1,32 @@
 export function DataBinding(){
     var category = ["Students", "Teachers", "Workers"];
+	let obj = {
+		Name: "Sudhakar",
+		Exp: 2,
+		Qualification: "BSc",
+		Cities: ["Hyd", "Chennai","Mumbai"],
+		Married: false,
+		Skills:{Java: 5, CSS: 3}
+	}
+	var items = new Map();
+	items.set(13, "It is roll number");
+	items.set("Name", 11);
+	
+	items.delete(13);
     return(
         <>
             <h2>Data Binding</h2>
+			<div className="btn-group-vertical">
+				{
+					category.map((cat) => 
+						<button key={cat} className="btn btn-danger mb-2">{cat}</button>
+					)
+				}
+			</div>
             <ol>
                 {
                     category.map((cat) =>
-                        <li>{cat}</li>
+                        <li key={cat}>{cat}</li>
                     )
                 }
             </ol>
@@ -14,7 +34,7 @@ export function DataBinding(){
             <select>
                 {
                     category.map(cat => 
-                        <option>{cat}</option>
+                        <option key={cat}>{cat}</option>
                     )
                 }
             </select>
@@ -22,7 +42,7 @@ export function DataBinding(){
             <ul className="list-unstyled">
                 {
                     category.map(cat => 
-                        <li>
+                        <li key={cat}>
                             <input type="checkbox"/> <label>{cat}</label>
                         </li>
                     )
@@ -38,13 +58,38 @@ export function DataBinding(){
                 <tbody>
                     {
                         category.map(cat =>     
-                            <tr>
-                                <td>{cat}</td>
+                            <tr key={cat}>
+                                <td className="d-flex justify-content-between">{cat} <button className="bi bi-trash btn btn-danger"></button></td>
                             </tr>
                         )
                     }
                 </tbody>
             </table>
+			
+			<label>Object Details</label>
+			<div>{obj.Name}</div>
+			<div>{(obj.Married) == true ? "Married" : "Unmarried"}</div>
+			<dt>Cities</dt>
+			<dd>
+				<ol>
+					{
+						obj.Cities.map(city => 
+							<li key={city}>{city}</li>
+						)
+					}
+				</ol>
+			</dd>
+			
+			<dt>Skills</dt>
+			<dd>
+				<ol>
+					{ obj.Skills.Java }
+				</ol>
+			</dd>
+			
+			<label>Map Properties</label>
+			<div>{items.get(13)}</div>
+			<div>{items.get("Name")}</div>
         </>
     )
 }
